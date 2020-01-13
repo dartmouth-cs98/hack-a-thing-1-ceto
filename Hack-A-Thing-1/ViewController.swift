d28e4cc7cc902e899dde7632f9ffcd1ceebeae31
Jason Ceto <jason.b.ceto.20@dartmouth.edu>
2020-01-13 02:30:57 -0500

    fixed previous commit

---
+++ Hack-A-Thing-1/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  Hack-A-Thing-1
+//
+//  Created by Jason Ceto on 1/13/20.
+//  Copyright © 2020 Jason Ceto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+        
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    var playerScore = 0
+    var compScore = 0
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    @IBAction func dealButtonTapped(_ sender: Any) {
+                
+        // grab a random integer for drawing cards (can be improved)
+        let leftNum = Int.random(in: 2...14)
+        let rightNum = Int.random(in:2...14)
+        
+        // update card images
+        leftImageView.image = UIImage(named: "card\(leftNum)")
+        rightImageView.image = UIImage(named: "card\(rightNum)")
+        
+        
+        // score round
+        if leftNum > rightNum {
+            
+            // player wins
+            playerScore += 1
+            
+            leftScoreLabel.text = String(playerScore)
+        
+        } else if rightNum > leftNum {
+            
+            // comp wins
+            compScore += 1
+            
+            rightScoreLabel.text = String(compScore)
+        }
+    }
+    
+
+
+}
+
